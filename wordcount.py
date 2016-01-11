681c6554f36150df745ae269b697dfe6d13d7692
Hackbright-student <info@hackbrightacademy.com>
2016-01-11 13:56:41 -0800

    Use Counter object to return counts without building dictionary.

--- wordcount.py
+++ wordcount.py
@@ -1,38 +1,30 @@
 # put your code here.
 import sys
-
+from collections import Counter 
 
 def count_unique_words(filename):
     """For any text file, prints dictionary of unique words and number of occurences."""
 
-    # words = []
-    dict_of_words = {}
+    words = []
+    # dict_of_words = {}
     file_data = open(filename)
     for line in file_data: 
-        line = line.rstrip()
+        line = line.strip()
         line = line.split()
-    #     words.extend(line.split(" "))
-    # file_data.close()
-
-    # unique_words = set(words)
-    # dict_of_words = {}
-
-    # for item in unique_words: 
-    #     count = words.count(item)
-    #     dict_of_words[item] = count
         for word in line:
-            word = word.strip('.,!?:; -"_')
+            word = word.strip('.,!?:; -"_\n')
             word = word.lower()
-            dict_of_words[word] = 1 + dict_of_words.get(word, 0)
+            # dict_of_words[word] = 1 + dict_of_words.get(word, 0)
+            if len(word) > 0:
+                words.append(word)
             # if word not in dict_of_words:
             #     dict_of_words[word] = 1
             # else:
             #     dict_of_words[word] += 1
     file_data.close()
-
-    for key, value in dict_of_words.iteritems():
+    word_counts = Counter(words)
+    for key, value in word_counts.iteritems():
         print key, value
 
 filename=sys.argv[1]
 count_unique_words(filename)
-# count_unique_words("test.txt")
